2a25c14368f6983b92f14628b01785633696e584
qasim munir <writingtoqasim@localhost.localdomain>
2023-08-25 18:00:10 +0500

    first commit

---
+++ dump/mydb.sql
@@ -0,0 +1,14 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE `Person` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `Person` (`id`, `name`) VALUES
+(1, 'qasim'),
+(2, 'qaiser'),
+(3, 'john'),
+(4, 'gill');
